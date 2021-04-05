61e7c40c305812f8ead631d9ab127924d4caa8c0
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-04-05 21:46:34 +0200

    semaphore marche

--- semaphores/main.c
+++ semaphores/main.c
@@ -4,36 +4,31 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <unistd.h>
+#include <string.h>
+#define SEM_KEY 420
 
-int main (int argc, char * argv[])
+int main ()
 {
-
-  int key;
-  int tache;
   int sem;
-  int init = 1;
   struct sembuf buf;
-  int i;
 
-  if( argc != 2){
-    printf("pb argument: numero cle \n");
-    exit(-1);
-  }
-  key = atoi(argv[1]);
-  
    /* creation ou attachement du semaphore */
-  sem=semget((key_t) key, 1, 0666| IPC_CREAT);
-  if(sem == -1){
-    printf("pb creation du semaphore de production\n");
-    exit(-1);
-  }
   
-  /* initialisation du semaphore par le consommateur */
-  if (semctl (sem, 0, SETVAL, init)== -1){
-      printf("pb initialisation semaphore de production");
+  if(-1 == (sem=semget((key_t) SEM_KEY, 1, 0666)) ){
+    printf("pb creation du semaphore de production. Creation du semaphore\n");
+    if(-1 == (sem=semget((key_t) SEM_KEY, 1, 0666 | IPC_CREAT)) ){
+        printf("Echec de la creation du semaphore\n");
+        exit(-1);
+    }
+      /* initialisation du semaphore*/
+    if (-1 == semctl(sem, 0, SETVAL, 1) ){
+      printf("pb initialisation semaphore");
       exit(-1);
+    }
+  
   }
   
+
   /* initialisation commune du buffer pour les operations sur le semaphore */
   buf.sem_num = 0;
   buf.sem_flg = 0;
@@ -42,25 +37,30 @@ int main (int argc, char * argv[])
   input[0] = '\0';
   int PID = getpid();
   while(1){
-      
+      //P(S)
+      printf("instant 0 : Semaphore = %d\n",semctl(sem, 0, GETVAL));
       buf.sem_op = -1;
       if (semop(sem, &buf, 1)){
 	    printf ("PID %d : pb attente\n",PID);
 	    exit(-1);
       }
+      printf("instant 1 : Semaphore = %d\n",semctl(sem, 0, GETVAL));
+
       /* attente d'une chaine de caracteres de l'utilisateur */
-      scanf("%s",input);
+      printf("Entrez une chaine : ");
+      //scanf("%s",input);
+      fgets(input, 99, stdin);
+      strtok(input, "\n");           /* Suppression du '\n' */
       printf("\nPID %d : Chaine : %s\n",PID,input);
-        
       
-      
+      //V(S)
       buf.sem_op = 1;
-      sleep(10);
       if (semop(sem, &buf, 1)){
 	    printf ("PID %d : pb liberation\n",PID);
 	    exit(-1);
         
       }     
+      printf("instant 2 : Semaphore = %d\n",semctl(sem, 0, GETVAL));
     }
   return 0;
 }
